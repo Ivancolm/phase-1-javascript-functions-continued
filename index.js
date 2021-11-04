175f42b8e0022a35c2b81be4b631d1848d9a9087
ivancolmenares <ivancolmenares@live.com>
2021-11-04 10:02:29 -0500

    commit tree 83953a5ae8d50db9caae92baae2f42380a8d8eed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun(){
+    console.log ()
+    if ("roller-skate"){
+    return 'This Saturday, I want to roller-skate!';
+}else if("bathe my dog"){
+    return 'This Saturday, I want to bathe my dog!';
+}
+}
+function mondayWork(b) {
+    if ('go to the office'){
+    return "This Monday, I will go to the office.";
+}else if("work from home"){
+    return "This Monday, I will work from home.";
+}
+}
+function wrapAdjective(){}
